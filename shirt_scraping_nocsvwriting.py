f9b824c7949d3e93842033b8a4f53cd966c7241e
Tomu Fujii <tomufujii@Macintosh.home>
2015-08-30 22:09:58 -0400

    added comments and cleaned up code

--- shirt_scraping_nocsvwriting.py
+++ shirt_scraping_nocsvwriting.py
@@ -2,27 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 
+# input is the jcrew dress shirts page
+# goes to every shirt and rtrieves the name, image url, and shirt url
+# then calls get_shirt_details to visit the shirt page to retrieve further data
+
 def get_shirts(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content)
 
-    shirt_data = soup.find_all("td", {"class": "arrayProdCell"})
+    # retrieve each shirt grid
+    shirts = soup.find_all("td", {"class": "arrayProdCell"})
 
+    # the counter and maxNum variables are only used to limit items for testing
     counter = 0;
     maxNum = 2
-    for item in shirt_data:
-        # only one want to preview 1 shirt
-        if counter <= maxNum:
-            shirt_name = item.find_all("li", {"id", "arrayProdName"})[0].text
-            shirt_img = item.find_all("img")[0]["src"]
-            shirt_page = item.find_all("a")[0]["href"]
+    for shirt in shirts:
+        if counter < maxNum:
+            shirt_name = shirt.find_all("li", {"id", "arrayProdName"})[0].text
+            shirt_img = shirt.find_all("img")[0]["src"]
+            shirt_page = shirt.find_all("a")[0]["href"]
+            #print to console for testing
             print shirt_name
             print shirt_img
             print shirt_page
+            # visit the shirt page to retrieve further data
             get_shirt_details(shirt_page)
             counter += 1
         else:
             break
+
+# input is the shirt page url
+# used to capture shirt details, such as description, technical details, etc.
 
 def get_shirt_details(url):
     r2 = requests.get(url)
@@ -55,4 +65,4 @@ def get_shirt_details(url):
     
 
 get_shirts("https://www.jcrew.com/mens_category/dressshirts.jsp")
-get_shirt_details("https://www.jcrew.com/mens_category/dressshirts/ludlowdress/PRD~99467/99467.jsp?color_name=fairweather-blue")
+# get_shirt_details("https://www.jcrew.com/mens_category/dressshirts/ludlowdress/PRD~99467/99467.jsp?color_name=fairweather-blue")
